eda1a392e54fb730a944398d7e1c4b938e6c5c5f
Nick Gundobin <ngundobin@gmail.com>
2021-01-05 19:01:09 -0500

    routes using json as database

--- itemsRoutes.js
+++ itemsRoutes.js
@@ -1,23 +1,34 @@
 const express = require('express');
+const fs = require('fs');
 const ExpressError = require('./expressError');
 
 const router = new express.Router();
 
-const items = require('./fakeDB');
-
+//const items = require('./fakeDB');
+const file = fs.readFileSync("shopping-list.json", "utf8");
+const items = file ? JSON.parse(file) : [];
+console.log(items);
 router.get('/',(req,res)=>{
     res.json({items})
 })
 
 router.post('/', function(req,res){
-    // const newId = ITEMS[ITEMS.length-1].id+1;
-    //const newItem = Object.assign({id:newId}, req.body);
-    // console.log(`Price: ${req.body.item}`);
-    // console.log(`Price: ${req.body.price}`)
-    const newItem = {name: req.body.name, price: req.body.price};
+    let newId = items.length? items[items.length-1].id+1:1;
+    // if(items.length) {
+    //     newId = items[items.length-1]+1;
+    // } else {
+    //     newId = 1;
+    // }
+    const newItem = Object.assign({ id: newId }, req.body);
+
     items.push(newItem);
-    res.status(201).json({
-        items: newItem
+    fs.writeFile("shopping-list.json", JSON.stringify(items), err => {
+        res.status(201).json({
+            status: "success",
+            items: {
+                item : newItem
+            }
+        })
     })
 })
 
@@ -34,9 +45,18 @@ router.patch('/:name', (req,res)=>{
     if (!item) {
         throw new ExpressError("Cannot find the item", 404);
     }
-    item.name = req.body.name;
-    item.price = req.body.price;
-    res.json({item: item})
+    if(req.body.name){
+        item.name = req.body.name;
+    }
+    if(req.body.price){
+        item.price = req.body.price;
+    }
+    fs.writeFile("shopping-list.json", JSON.stringify(items), err => {
+        res.status(201).json({
+            status: "success",
+            message: "Successfully updated"
+        })
+    })
 })
 
 router.delete('/:name',(req,res)=>{
@@ -45,7 +65,12 @@ router.delete('/:name',(req,res)=>{
         throw new ExpressError("Cannot find the item", 404);
     }
     items.splice(item,1)
-    res.json({message:"Deleted"})
+    fs.writeFile("shopping-list.json", JSON.stringify(items), err => {
+        res.status(201).json({
+            status: "success",
+            message: "Successfully deleted"
+        })
+    })
 })
 
 module.exports = router;
